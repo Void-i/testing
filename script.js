bd34e3b086c661e29d2c9ca93ac4d6bdd3b3f0f6
derpthebass <ramboxp@live.com>
2013-02-21 21:06:46 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+var div = document.getElementById("button-vote-positive");
+        div.parentNode.removeChild(div);
+
+ $('#dj-booth').prepend('<div id="button-vote-negative" style="cursor: default; background-image: url(http://i.imgur.com/gNx7ly2t.png); left: 410px;"></div>');
